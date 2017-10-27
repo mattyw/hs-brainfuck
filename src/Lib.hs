87ace8bbf215ce6e570ad8469bc2919f49c894ad
mattyw <gh@mattyw.net>
2017-10-27 23:02:29 +0100

    simplify eval

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,13 +19,14 @@ run code = run' (Program code 0 "" 0 "")
 run' :: Program -> String
 run' prog = ""
 
-eval :: Program -> Char -> Program
-eval (Program code cPos tape tPos out) '+' = Program code cPos tape (tPos+1) out
-eval (Program code cPos tape tPos out) '-' = Program code cPos tape (tPos-1) out
-eval (Program code cPos tape tPos out) '>' = Program code cPos (alterTape tape tPos next) tPos out
-eval (Program code cPos tape tPos out) '<' = Program code cPos (alterTape tape tPos prev) tPos out
-eval (Program code cPos tape tPos out) '.' = Program code cPos tape tPos (out ++ [tape !! tPos])
-eval (Program code cPos tape tPos out) ',' = Program code cPos (alterTape tape tPos id c) tPos out
+eval :: Program -> Program
+eval (Program ('+':code) cPos tape tPos out) = Program code cPos tape (tPos+1) out
+eval (Program ('-':code) cPos tape tPos out) = Program code cPos tape (tPos-1) out
+eval (Program ('>':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos next) tPos out
+eval (Program ('<':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos prev) tPos out
+eval (Program ('.':code) cPos tape tPos out) = Program code cPos tape tPos (out ++ [tape !! tPos])
+-- Don't understand what to put onto the tape, need to read about brain fuck
+eval (Program (',':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos id c) tPos out
 
 alterTape :: String -> Int -> (Char -> Char) -> String
 alterTape "" _ _ = ""
