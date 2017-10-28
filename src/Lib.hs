c9e44636465cbc553462b44bca629f0d432222c8
mattyw <gh@mattyw.net>
2017-10-28 22:30:48 +0100

    not working, think i need input

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,7 @@ module Lib
     , alterTape
     , next
     , prev
+    , put
     ) where
 
 import Data.Char
@@ -10,23 +11,32 @@ import Data.Char
 next c = chr (ord c + 1)
 prev c = chr (ord c -1 )
 
-data Program = Program String Int String Int String deriving (Show)
+data Program = Program String Int String Int String
+
+-- TODO we can remove out
+instance Show Program where
+    show (Program _ _ tape _ out) = "output: " ++ tape ++ " " ++ out
 
 
 run :: String -> String
 run code = run' (Program code 0 "" 0 "")
 
 run' :: Program -> String
-run' prog = ""
+run' prog = show $ eval prog
 
 eval :: Program -> Program
-eval (Program ('+':code) cPos tape tPos out) = Program code cPos tape (tPos+1) out
-eval (Program ('-':code) cPos tape tPos out) = Program code cPos tape (tPos-1) out
-eval (Program ('>':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos next) tPos out
-eval (Program ('<':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos prev) tPos out
+eval (Program ('>':code) cPos tape tPos out) = Program code cPos tape (tPos+1) out
+eval (Program ('<':code) cPos tape tPos out) = Program code cPos tape (tPos-1) out
+eval (Program ('+':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos next) tPos out
+eval (Program ('-':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos prev) tPos out
 eval (Program ('.':code) cPos tape tPos out) = Program code cPos tape tPos (out ++ [tape !! tPos])
--- Don't understand what to put onto the tape, need to read about brain fuck
-eval (Program (',':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos id c) tPos out
+eval (Program (',':code) cPos tape tPos out) = Program code cPos (alterTape tape tPos (put $ tape !! tPos)) tPos out
+-- Ignore all else
+eval (Program (_:code) cPos tape tPos out) = Program code cPos tape tPos out
+
+
+put :: Char -> Char -> Char
+put x _ = x
 
 alterTape :: String -> Int -> (Char -> Char) -> String
 alterTape "" _ _ = ""
