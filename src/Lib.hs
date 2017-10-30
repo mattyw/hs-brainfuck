c58df32cc9f1d79d38ad29b2a091ee8d78b6e3a3
mattyw <gh@mattyw.net>
2017-10-30 12:26:23 +0000

    fix jumps

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,17 +3,26 @@ module Lib
     , alterTape
     , next
     , prev
+    , jumpForwardPos
+    , jumpBackwardPos
+    , tape
     , put
     , parens
-    , helloWorld
     , otherParen
+    , Program (Program)
+    , eval
     ) where
 
 import Data.Char
 import Data.List
 import Data.Maybe
 
+next :: Char -> Char
+next '\255' = chr 0
 next c = chr (ord c + 1)
+
+prev :: Char -> Char
+prev '\0' = chr 255
 prev c = chr (ord c -1 )
 
 data Program = Program String String Int String Int String String
@@ -21,6 +30,18 @@ data Program = Program String String Int String Int String String
 -- TODO we can remove out
 instance Show Program where
     show (Program _ _ _ _ _ _ out) = out
+
+instance Eq Program where
+    (==) (Program a1 a2 a3 a4 a5 a6 a7)
+         (Program b1 b2 b3 b4 b5 b6 b7) = a1 == b1 &&
+                                          a2 == b2 &&
+                                          a3 == b3 &&
+                                          a4 == b4 &&
+                                          a5 == b5 &&
+                                          a6 == b6 &&
+                                          a7 == b7
+tape :: Program -> String
+tape (Program _ _ _ t _ _ _) = t
 
 run :: String -> String
 run code = run' (Program code code 0 (take 1000 $ repeat '\0') 0 "input" "")
@@ -40,30 +61,30 @@ eval (Program (',':code) allCode cPos tape tPos (i:inp) out) = eval $ Program co
 -- Jumps
 eval (Program ('[':code) allCode cPos tape tPos inp out) = eval $ Program newCode allCode newPos tape tPos inp out
     where
-        newPos = jumpForwardPos allCode cPos
+        newPos = jumpForwardPos (parens allCode) tape cPos
         newCode = drop newPos allCode
 eval (Program (']':code) allCode cPos tape tPos inp out) = eval $ Program newCode allCode newPos tape tPos inp out
     where
-        newPos = jumpBackwardPos allCode cPos
+        newPos = jumpBackwardPos (parens allCode) tape cPos
         newCode = drop newPos allCode
 -- Ignore all else
 eval (Program (_:code) allCode cPos tape tPos inp out) = eval $ Program code allCode (cPos+1) tape tPos inp out
 
-jumpForwardPos :: String -> Int -> Int
-jumpForwardPos tape pos =
+jumpForwardPos :: [(Int,Int)] -> String -> Int -> Int
+jumpForwardPos parens tape pos =
     if (valueOnTape tape pos) == '\0'
-           then  (index+1)
-           else pos
+           then (index+1)
+           else (pos+1)
     where
-        index = otherParen (parens tape) pos
+        index = otherParen parens pos
 
-jumpBackwardPos :: String -> Int -> Int
-jumpBackwardPos tape pos =
+jumpBackwardPos :: [(Int,Int)] -> String -> Int -> Int
+jumpBackwardPos parens tape pos =
     if (valueOnTape tape pos) /= '\0'
-           then  (index+1)
-           else pos
+           then (index+1)
+           else (pos+1)
     where
-        index = otherParen (parens tape) pos
+        index = otherParen parens pos
 
 valueOnTape :: String -> Int -> Char
 valueOnTape tape pos
@@ -86,6 +107,7 @@ findParen (']':xs) idx (h:stck) acc = findParen xs (idx+1) stck ((h, idx) : acc)
 findParen ('[':xs) idx stck acc = findParen xs (idx+1) (idx : stck) acc
 findParen (x:xs) idx stck acc = findParen xs (idx+1) stck acc
 
+-- TODO the tape should be Ints or Bytes
 parens :: String -> [(Int,Int)]
 parens s = findParen s 0 [] []
 
@@ -95,5 +117,3 @@ otherParen ((a,b):xs) n
     | a == n = b
     | b == n = a
     | otherwise = otherParen xs n
-
-helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,17 +8,40 @@ testMatch = TestCase (do
     let out = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
     let parens = [(8,48),(43, 45),(14, 33)]
     assertEqual "Test match" parens (Lib.parens out)
-    assertEqual "Find other" 48 (Lib.otherParen parens 8)
-    assertEqual "Find other" 43 (Lib.otherParen parens 45)
+    assertEqual "Find other" 48 (otherParen parens 8)
+    assertEqual "Find other" 43 (otherParen parens 45)
     )
 testRun = TestCase (do
     let bang = "+++++++++++++++++++++++++++++++++.+."
     let program = "++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++-<+++++++++++++++++++++++++++++++++++++++++++++++++++-.>>,."
+    let helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+    let onejump = "[+++++++++++++++++++++++++++++++++.]+++++++++++++++++++++++++++++++++."
     assertEqual "Test bang" "!\"" (run bang)
     assertEqual "Test 4fi" "4fi" (run program)
+    assertEqual "Test " "!" (run onejump)
+    assertEqual "Test " "Hello World!" (run helloWorld)
+    )
+testJumpPos = TestCase (do
+    let jump = "[+]++"
+    assertEqual "jump pos" 3 (jumpForwardPos (Lib.parens jump) "" 0)
+    assertEqual "jump pos" 1 (jumpForwardPos (Lib.parens jump) "1" 0)
+    assertEqual "jump pos" 3 (jumpBackwardPos (Lib.parens jump) "" 2)
+    assertEqual "jump pos" 1 (jumpBackwardPos (Lib.parens jump) "1" 2)
+    )
+testEval = TestCase (do
+    let jump = "[+]++"
+    let prog = (Program jump jump 0 (take 10 $ repeat '\0') 0 "" "")
+    let expected = (Program jump jump 0 "\STX\0\0\0\0\0\0\0\0\0" 0 "" "")
+    assertEqual "jump result" (tape expected) (tape (eval prog))
+    let helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+    let prog = (Program helloWorld helloWorld 0 (take 10 $ repeat '\0') 0 "" "")
+    let expected = (Program helloWorld helloWorld 0 "\STX\0\0\0\0\0\0\0\0\0" 0 "" "")
+    assertEqual "helloworld result" (tape expected) (tape (eval prog))
     )
 tests = TestList [
     testMatch
+    , testJumpPos
+    , testEval
     , testRun
     ]
 main :: IO ()
